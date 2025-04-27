19844f3af88e95f70a4ed528912fd16d4491c9d6
Piotr Skrobski <234796@edu.p.lodz.pl>
2025-04-27 18:31:41 +0200

    feat(Docker): Add Ollama local model to Docker

---
+++ ollama-entrypoint.sh
@@ -0,0 +1,17 @@
+# https://stackoverflow.com/questions/78500319/how-to-pull-model-automatically-with-container-creation
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve LLAMA3 model..."
+ollama pull llama3
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
